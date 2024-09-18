9c645c571859882d062658eed02375173303ead4
Jourvence <117796287+Vinqueire@users.noreply.github.com>
2024-09-18 22:25:00 +0200

    commit

---
+++ script.js
@@ -0,0 +1,239 @@
+var a1 = 0;
+var a2 = 0;
+var a3 = 0;
+var a4 = 0;
+var a5 = 0;
+var a6 = 0;
+var a7 = 0;
+var a8 = 0;
+var a9 = 0;
+var a10 = 0;
+var a11 = 0;
+var a12 = 0;
+var a13 = 0;
+var a14 = 0;
+var a15 = 0;
+var a16 = 0;
+var a17 = 0;
+var a18 = 0;
+
+function img1(){
+    if (a1 == 0){
+        a1 = 1;
+        document.getElementById("1").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a1 = 0;
+        document.getElementById("1").src = "img/img1.jpg"
+        console.log('0');
+    }
+        
+}
+function img2(){
+    if (a2 == 0){
+        a2 = 1;
+        document.getElementById("2").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a1 = 0;
+        document.getElementById("2").src = "img/img2.jpg"
+        console.log('0');
+    }
+        
+}
+function img3(){
+    if (a3 == 0){
+        a3 = 1;
+        document.getElementById("3").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a1 = 0;
+        document.getElementById("3").src = "img/img3.jpg"
+        console.log('0');
+    }
+}
+function img4(){
+    if (a4 == 0){
+        a4 = 1;
+        document.getElementById("4").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a4 = 0;
+        document.getElementById("4").src = "img/img11.jpg"
+        console.log('0');
+    }
+}
+function img5(){
+    if (a5 == 0){
+        a5 = 1;
+        document.getElementById("5").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a5 = 0;
+        document.getElementById("5").src = "img/img12.jpg"
+        console.log('0');
+    }
+}
+function img6(){
+    if (a6 == 0){
+        a6 = 1;
+        document.getElementById("6").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a6 = 0;
+        document.getElementById("6").src = "img/img13.jpg"
+        console.log('0');
+    }
+}
+function img7(){
+    if (a7 == 0){
+        a7 = 1;
+        document.getElementById("7").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a7 = 0;
+        document.getElementById("7").src = "img/img4.jpg"
+        console.log('0');
+    }
+}
+
+function img8(){
+    if (a8 == 0){
+        a8 = 1;
+        document.getElementById("8").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a8 = 0;
+        document.getElementById("8").src = "img/img5.jpg"
+        console.log('0');
+    }
+}
+function img9(){
+    if (a9 == 0){
+        a9 = 1;
+        document.getElementById("9").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a9 = 0;
+        document.getElementById("9").src = "img/img6.jpg"
+        console.log('0');
+    }
+}
+
+function img10(){
+    if (a10 == 0){
+        a10 = 1;
+        document.getElementById("10").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a10 = 0;
+        document.getElementById("10").src = "img/img14.jpg"
+        console.log('0');
+    }
+}
+function img11(){
+    if (a11 == 0){
+        a11 = 1;
+        document.getElementById("11").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a11 = 0;
+        document.getElementById("11").src = "img/img15.jpg"
+        console.log('0');
+    }
+}
+function img12(){
+    if (a12 == 0){
+        a12 = 1;
+        document.getElementById("12").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a12 = 0;
+        document.getElementById("12").src = "img/img16.jpg"
+        console.log('0');
+    }
+}
+function img13(){
+    if (a13 == 0){
+        a13 = 1;
+        document.getElementById("13").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a13 = 0;
+        document.getElementById("13").src = "img/img8.jpg"
+        console.log('0');
+    }
+}
+function img14(){
+    if (a14 == 0){
+        a14 = 1;
+        document.getElementById("14").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a14 = 0;
+        document.getElementById("14").src = "img/img9.jpg"
+        console.log('0');
+    }
+}
+function img15(){
+    if (a15 == 0){
+        a15 = 1;
+        document.getElementById("15").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a15 = 0;
+        document.getElementById("15").src = "img/img10.jpg"
+        console.log('0');
+    }
+}
+function img16(){
+    if (a16 == 0){
+        a16 = 1;
+        document.getElementById("16").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a16 = 0;
+        document.getElementById("16").src = "img/img17.jpg"
+        console.log('0');
+    }
+}
+function img17(){
+    if (a17 == 0){
+        a17 = 1;
+        document.getElementById("17").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a17 = 0;
+        document.getElementById("17").src = "img/img19.jpg"
+        console.log('0');
+    }
+}
+function img18(){
+    if (a18 == 0){
+        a18 = 1;
+        document.getElementById("18").src = "img/black.jpg"
+        console.log('1');
+    }
+    else{
+        a18 = 0;
+        document.getElementById("18").src = "img/img20.jpg"
+        console.log('0');
+    }
+}
